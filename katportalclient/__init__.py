7f62b98a91e1b7974cb0047e28ce00eb8f98e0d1
Francois Joubert <fjoubert@ska.ac.za>
2016-08-05 14:27:05 +0200

    versions for local packages

--- katportalclient/__init__.py
+++ katportalclient/__init__.py
@@ -1,2 +1,13 @@
 from client import KATPortalClient
 from request import JSONRPCRequest
+
+# BEGIN VERSION CHECK
+# Get package version when locally imported from repo or via -e develop install
+try:
+    import katversion as _katversion
+except ImportError:
+    import time as _time
+    __version__ = "0.0+unknown.{}".format(_time.strftime('%Y%m%d%H%M'))
+else:
+    __version__ = _katversion.get_version(__path__[0])
+# END VERSION CHECK
