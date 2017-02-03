b6e5e01f3b305baa1f05382241ab8c1971d9cfc5
Anton Joubert <ajoubert@ska.ac.za>
2017-02-03 15:00:29 +0200

    Improve get schedule block info example

--- examples/get_schedule_block_info.py
+++ examples/get_schedule_block_info.py
@@ -14,6 +14,7 @@ specific subarray when initialising the KATPortalClient, as schedule blocks
 are assigned to specific subarrays.
 """
 import logging
+import argparse
 
 import tornado.gen
 
@@ -29,16 +30,16 @@ def main():
     # Change URL to point to a valid portal node.  Subarray can be 1 to 4.
     # Note: if on_update_callback is set to None, then we cannot use the
     #       KATPortalClient.connect() method (i.e. no websocket access).
-    subarray_number = 1
-    portal_client = KATPortalClient('http://portal.mkat/api/client/{}'.
-                                    format(subarray_number),
+    portal_client = KATPortalClient('http://{}/api/client/{}'.
+                                    format(args.host, args.sub_nr),
                                     on_update_callback=None, logger=logger)
 
     # Get the IDs of schedule blocks assigned to the subarray specified above.
     sb_ids = yield portal_client.schedule_blocks_assigned()
-    print "\nSchedule block IDs:", sb_ids
+    print "\nSchedule block IDs on subarray {}\n{}".format(args.sub_nr, sb_ids)
     # Example output:
-    #   Schedule block IDs: [u'20161010-0001', u'20161010-0002', u'20161010-0003']
+    #   Schedule block IDs on subarray 1:
+    #   [u'20161010-0001', u'20161010-0002', u'20161010-0003']
 
     # Fetch the details for one of the schedule blocks found.
     if len(sb_ids) > 0:
@@ -64,6 +65,29 @@ def main():
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Download schedule block info for a subarray and print to stdout.")
+    parser.add_argument(
+        '--host',
+        default='127.0.0.1',
+        help="hostname or IP of the portal server (default: %(default)s).")
+    parser.add_argument(
+        '-s', '--sub_nr',
+        default='1',
+        type=int,
+        help="subarray number (1, 2, 3, or 4) to request schedule for "
+             "(default: %(default)s).")
+    parser.add_argument(
+        '-v', '--verbose',
+        dest='verbose', action="store_true",
+        default=False,
+        help="provide extremely verbose output.")
+    args = parser.parse_args()
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.WARNING)
+
     # Start up the tornado IO loop.
     # Only a single function to run once, so use run_sync() instead of start()
     io_loop = tornado.ioloop.IOLoop.current()
